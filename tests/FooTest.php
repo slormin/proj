f5b3a8d7752e89e37aff473ef9f83d3fe32d7260
Sergio Lorenzo <sergiolorenzo@salumedia.com>
2021-03-15 04:52:58 +0100

    Initial commit

--- tests/FooTest.php
+++ tests/FooTest.php
@@ -17,11 +17,6 @@ class BuiltinTest extends TestCase
 
 
 
-
-
-        $curl_exec = $this->getFunctionMock(__NAMESPACE__, "curl_exec");
-        $curl_exec->expects($this->once())->willReturn("body");
-
         $ch = curl_init();
         $this->assertEquals('{"2":{"sku":"000003","name":"Ashlington leather ankle boots","category":"boots","price":{"original":71000,"final":60350,"discountPercentage":"15%","currency":"EUR"}},"4":{"sku":"000005","name":"Nathane leather sneakers","category":"sneakers","price":{"original":59000,"final":59000,"discountPercentage":null,"currency":"EUR"}}}', curl_exec($ch));
         curl_close($ch);
